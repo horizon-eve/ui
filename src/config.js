714af947e191febe0388f9ce3558c20f7001de3b
Oleksandr Minaiev <oleksminaiev@gmail.com>
2022-01-17 17:37:59 -0500

    update esi url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 module.exports = {
   API_BASE_URL: 'https://api.racopub.com',
   AUTH_BASE_URL: 'https://auth.racopub.com',
-//  ESI_BASE_URL: 'https://esi.racopub.com'
-  ESI_BASE_URL: 'http://localhost:3000'
+  ESI_BASE_URL: 'https://esi.racopub.com'
 }
